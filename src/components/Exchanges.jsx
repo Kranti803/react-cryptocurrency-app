7b0e3b87e361f2a9011c0fde0ab928c29cb66b10
Kranti Kumar Shah <Krantikumar803@gmail.com>
2022-12-22 14:01:30 +0545

    fixed exchanges page..

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Exchanges = () => {
   return (
-    <div>Exchanges</div>
+    <div className='exchanges'>
+      <h1>404 Error...</h1>
+      <button><a href="/">Retun to Home</a></button>
+    </div>
   )
 }
 
